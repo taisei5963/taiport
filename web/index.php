16b4104be5fced44818813fd8e6d2fedd1977d17
taisei5963 <taisei.basket.5963@gmail.com>
2024-09-14 17:26:07 +0900

    ポート番号修正

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@ try {
   $request_path = $_REQUEST['path'];
 
   // ポート番号の環境変数対応
-  $port = $_SERVER['PORT'] ?? 8080;
+  $port = $_SERVER['PORT'] ?? 9000;
 
   // 文頭にスラッシュが付与されていない場合場合は強制的に付与する
   if(!str_starts_with($request_path, '/')) {
