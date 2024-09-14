76591908616ab1466ce788a985d59fa4ba746382
taisei5963 <taisei.basket.5963@gmail.com>
2024-09-14 17:19:53 +0900

    リファクタリング

--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
+
 //読み込み用ファイル宣言
 require_once(dirname(__FILE__).'/mapping.php');
 
@@ -6,21 +7,17 @@ try {
   // セッション開始
   session_start();
 
-  // $pdo = connect_mysql();
-
   $err = array();
 
   $request_path = $_REQUEST['path'];
+
+  // ポート番号の環境変数対応
+  $port = $_SERVER['PORT'] ?? 8080;
 
   // 文頭にスラッシュが付与されていない場合場合は強制的に付与する
   if(!str_starts_with($request_path, '/')) {
     $request_path = '/'.$request_path;
   }
-
-  // 末尾にスラッシュが付与されていない場合は強制的に付与する
-  // if(!str_ends_with($request_path, '/')) {
-  //   $request_path .= '/';
-  // }
 
   // mapping.phpに従って対象PHPに処理を移譲
   if (isset($url_list[$request_path])) {
